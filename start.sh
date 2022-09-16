e49b9aea1ef5180e40f9cf95fb43838956d8f252
djfaizp <modan.mohammad@gmail.com>
2022-09-16 12:05:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/illuzX/AtwFilt /AtwFilt
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AtwFilt
-fi
 cd /AtwFilt
 pip3 install -U -r requirements.txt
 echo "AtwFilt bot runing...."
